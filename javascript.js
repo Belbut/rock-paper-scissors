fbd1f9894823de0e87ab1e43e2b0064eed6c7eb0
Belbut <andreferreirabelbut@gmail.com>
2022-09-25 17:42:08 +0100

    Add buttons and score trackers

--- javascript.js
+++ javascript.js
@@ -1,3 +1,13 @@
+const gameResult = document.querySelector("#game_result");
+let playerScore = document.querySelector("#player_score");
+let computerScore = document.querySelector("#computer_score");
+
+const container = document.querySelector("#results");
+const actionMade = document.createElement("div");
+container.append(actionMade);
+
+
+
 function getComputerChoice() {
     //the const randomCode is a random number between 1 and 3;
     const randomCode = Math.floor(Math.random() * 3 + 1);
@@ -15,68 +25,40 @@ function getComputerChoice() {
     }
 }
 
+
 function playRound(playerSelection, computerSelection) {
     const lowCasePlayerSelection = playerSelection.toLowerCase();
 
     if (lowCasePlayerSelection === computerSelection) {
+        gameResult.textContent = "Draw";
         return "draw";
     } else {
         if ((lowCasePlayerSelection === "rock" && computerSelection === "scissor") ||
             (lowCasePlayerSelection === "paper" && computerSelection === "rock") ||
             (lowCasePlayerSelection === "scissor" && computerSelection === "paper")
         ) {
+            gameResult.textContent = "Player Won";
+            playerScore.textContent= parseInt(playerScore.textContent)+1;
             return "player won";
         }
-        else return "player loss"
-    }
-}
-
-function game() {
-
-    //initialize 2 variables that stores the points of previous games
-    let playerPoints = 0;
-    let computerPoints = 0;
-    //use a for loop to play 5 rounds.
-    for (round = 0; round < 5; round++) {
-        //    in each round:
-        //    create a variable that get user input using prompt() function
-        playerChoice = window.prompt("Pick your weapon carefully");
-        //    create a variable that stores the random computerChoice
-        computerChoice = getComputerChoice();
-        //    Create a  variable to store a dynamic String to log on the end of each round
-        logString = "This is round" + (round + 1) + " , Player picked:" + playerChoice + " and computer:" + computerChoice + " result is";
-
-        //    should playRound() using the 2 variables and give the point to the winner
-        switch (playRound(playerChoice,computerChoice)) {
-            case "draw":
-                logString = logString + " a draw";
-                break;
-
-            case "player won":
-                logString = logString + " a point for the human";
-                playerPoints++;
-                break;
-
-            case "player loss":
-                logString = logString + " a point for the machine";
-                computerPoints++;
-                break;
+        else {
+            gameResult.textContent = "Computer Won";
+            computerScore.textContent= parseInt(computerScore.textContent)+1;
+            return "player loss"
         }
-        //    log the message of who won;
-        console.log(logString)
     }
-
-    //after the 5 round present the winner
-    if (playerPoints === computerPoints) {
-        console.log("No one own");
-    } else if (playerPoints > computerPoints) {
-        console.log("HUMAN Won with " + playerPoints + " points");
-    } else {
-        console.log("MACHINE Won with " + computerPoints + " points");
-
-    }
-
 }
 
 
-game();
+
+const buttons = document.querySelectorAll('button');
+buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+        const playerWeapon = button.id;
+        const computerWeapon = getComputerChoice();
+        actionMade.textContent = `You picked ${playerWeapon} and computer used ${computerWeapon}`;
+        gameResult.textContent = playRound(playerWeapon, computerWeapon);
+    });
+});
+
+
